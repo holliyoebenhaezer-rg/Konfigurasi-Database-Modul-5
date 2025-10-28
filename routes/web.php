1416379eb4d6568802686ee4c622e2c1cea1ed64
Holliyo Eben Haezer T <holliyoebenhaezer@gmail.com>
2025-10-29 02:41:43 +0700

    Konfigurasi Database

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-use App\Http\Controllers\StudentController;
-
-Route::get('/students', [StudentController::class, 'index'])->name('students.index');
-
-use App\Http\Controllers\DashboardTubesController;
-
-Route::get('/pandeglangtrip', [DashboardTubesController::class, 'index'])->name('tubes.dashboard');
-Route::view('/pandeglangtrip/tur', 'tubes.tur_tubes')->name('tubes.tur');
-Route::view('/pandeglangtrip/wisata', 'tubes.wisata_tubes')->name('tubes.wisata');
-Route::view('/pandeglangtrip/faq', 'tubes.faq_tubes')->name('tubes.faq');
+Route::get('/form', [MahasiswaController::class, 'form']);
+Route::post('/simpan', [MahasiswaController::class, 'simpan']);
+Route::get('/daftar-mahasiswa', [MahasiswaController::class, 'daftar']);
